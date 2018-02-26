8ebc48c817aa8f414ac00ac9ceb6271887f35006
Feng-iMac <Feng-iMac@Feng-iMacs-MacBook-Pro.local>
2018-02-26 17:33:22 -0600

    Initial Commit

---
+++ currentLocation2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  currentLocation2
+//
+//  Created by Feng-iMac on 2/26/18.
+//  Copyright © 2018 Feng-iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
